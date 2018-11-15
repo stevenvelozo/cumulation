b5c3f41c5f50b0d1d2a6e061f20b0e14943cf471
Cesar Vargas <limitlis@gmail.com>
2018-11-15 12:50:02 -0800

    missing semi-colon

--- source/Cumulation-Browser-Shim.js
+++ source/Cumulation-Browser-Shim.js
@@ -8,5 +8,5 @@
 */
 
 // Load the cumulation module into the browser global automatically.
-window.Cumulation = require('./Cumulation.js')
+window.Cumulation = require('./Cumulation.js');
 module.exports = window.Cumulation;
